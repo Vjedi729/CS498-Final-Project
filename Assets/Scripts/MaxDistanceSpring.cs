b3b8f714491c4734ada362160d068571d6fff2c2
George Aleksandrovich <georgealeks@hotmail.com>
2018-05-03 02:09:05 -0500

    WIP puzzle snapping
    TODO fix the actual position that it tries to connect together (provide input coords? or better specify rigidbody?)

--- Assets/Scripts/MaxDistanceSpring.cs
+++ Assets/Scripts/MaxDistanceSpring.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class MaxDistanceSpring : MonoBehaviour {
 
-	SpringJoint sj;
+	public GameObject connectedBody;
+	private Rigidbody connectedRB;
+
+	private FixedJoint fj;
+	private SpringJoint sj;
 	public float maxDistance = 0.25f;
+	public float minDistance = 0.05f;
+
+	private bool isFixed = false;
 
 	// Use this for initialization
 	void Start () {
-		sj = GetComponent<SpringJoint> ();
+		//sj = GetComponent<SpringJoint> ();
+		connectedRB = connectedBody.GetComponent<Rigidbody>();
+
+		float distance = (transform.position - connectedBody.transform.position).magnitude;
+		if (distance < minDistance) {
+			isFixed = true;
+			this.gameObject.AddComponent<FixedJoint> ();
+			fj = GetComponent<FixedJoint> ();
+			fj.connectedBody = connectedRB;
+		} else {
+			this.gameObject.AddComponent<SpringJoint> ();
+			sj = GetComponent<SpringJoint> ();
+			sj.connectedBody = connectedRB;
+
+			sj.anchor = new Vector3 (0.25f, 0, 0);
+			sj.autoConfigureConnectedAnchor = false;
+			sj.connectedAnchor = new Vector3 (1f, 0, 0);
+			sj.spring = 500f;
+			sj.enableCollision = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((transform.position - sj.connectedBody.transform.position).magnitude > maxDistance) {
+		float distance = (transform.position - sj.connectedBody.transform.position).magnitude;
+		Debug.Log (distance);
+
+		if (distance > maxDistance) {
 			sj.minDistance = 1000f;
 		} else {
-			sj.minDistance = sj.maxDistance = 0f;
+			sj.minDistance = 0f;
+			if (distance < minDistance) {
+				//GameObject.Destroy (sj);
+				this.gameObject.AddComponent<FixedJoint> ();
+				fj = GetComponent<FixedJoint> ();
+				fj.connectedBody = sj.connectedBody;
+
+				GameObject.Destroy (sj);
+			}
 		}
 	}
 }
