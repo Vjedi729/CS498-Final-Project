0a850ec24d525a8d97bd4cc4c728871e8040f2f4
Goggles <vjedi729@gmail.com>
2018-05-11 14:39:00 -0500

    Added Set Position in Room
    Added Announcer Objects, which run incoming void->void delegates only after the Announce function is called.
    
    Added Announcer functionality to setPositionInRoom script, so the game doesn't freeze.
    
    Added a demo for this called Advanced Scaling Demo
    
    Added the Lever2 prefab (soon to replace the lever prefab) which can be dynamically moved without breaking.

---
+++ FinalProject/Assets/Scripts/SetPositionToRoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositionToRoom : MonoBehaviour {
+
+	public Vector3 inRoomPosition;
+	public Vector3 offset;
+
+	public AutomaticRoomScaler roomScaler;
+
+	#if UNITY_EDITOR
+	public bool resetPos;
+	#endif
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR
+		Debug.LogFormat("Registering for Room Done: {0}", gameObject.name);
+		#endif
+		roomScaler.roomDone.Register (resetPosition);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		// Code for allowing testing of values for position/offset
+		if (resetPos) {
+			resetPosition ();
+			resetPos = false;
+		}
+		#endif
+
+		//Debug.Log (transform.position);
+	}
+
+	void resetPosition(){
+		#if UNITY_EDITOR
+		Debug.LogFormat("Setting Position To Room: {0}", gameObject.name);
+		#endif
+
+		transform.position = roomScaler.getPosInRoom (inRoomPosition) + offset;
+	}
+}
